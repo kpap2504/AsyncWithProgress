3864e042677631385f08808a5a6a6475e474d66f
kpap2504@gmail.com <kpap2504@gmail.com>
2019-09-07 17:30:50 +0300

    2019-09

---
+++ AsyncWithProgress/FrmAsyncProgress.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmAsyncProgress
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.lblPercent = New System.Windows.Forms.Label()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.barFileProgress = New System.Windows.Forms.ProgressBar()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(31, 38)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'lblPercent
+        '
+        Me.lblPercent.AutoSize = True
+        Me.lblPercent.Location = New System.Drawing.Point(28, 119)
+        Me.lblPercent.Name = "lblPercent"
+        Me.lblPercent.Size = New System.Drawing.Size(39, 13)
+        Me.lblPercent.TabIndex = 1
+        Me.lblPercent.Text = "Label1"
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(31, 12)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(378, 20)
+        Me.txtPath.TabIndex = 2
+        '
+        'barFileProgress
+        '
+        Me.barFileProgress.Location = New System.Drawing.Point(31, 81)
+        Me.barFileProgress.Name = "barFileProgress"
+        Me.barFileProgress.Size = New System.Drawing.Size(378, 23)
+        Me.barFileProgress.TabIndex = 3
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(334, 38)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 0
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'FrmAsyncProgress
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(424, 138)
+        Me.Controls.Add(Me.barFileProgress)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.lblPercent)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnStart)
+        Me.Name = "FrmAsyncProgress"
+        Me.Text = "Async Progress"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents lblPercent As Label
+    Friend WithEvents txtPath As TextBox
+    Friend WithEvents barFileProgress As ProgressBar
+    Friend WithEvents btnCancel As Button
+End Class
---
+++ AsyncWithProgress/FrmAsyncProgress.vb
@@ -0,0 +1,114 @@
+﻿Imports System.IO
+Imports System.Threading
+
+Public Class FrmAsyncProgress
+    ''' <summary>
+    ''' https_://www.dreamincode.net/forums/topic/388819-asyncawait-with-progressbar-and-cancellation/
+    ''' </summary>
+    ''' 
+    Private tokenSource As CancellationTokenSource
+
+    Private Sub FrmAsyncProgress_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        barFileProgress.Minimum = 0
+        barFileProgress.Maximum = 100
+        btnCancel.Enabled = False
+    End Sub
+
+    Private Async Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If String.IsNullOrWhiteSpace(txtPath.Text) Then
+            MessageBox.Show("Provide a location first.", "Location")
+            Exit Sub
+        End If
+        Dim sLocation As String = txtPath.Text.Trim()
+        If Not Directory.Exists(sLocation) Then
+            MessageBox.Show("Directory doesn't exist.", "Location")
+            Exit Sub
+        End If
+
+        Dim progressIndicator = New Progress(Of Integer)(AddressOf UpdateProgress)
+
+        btnStart.Enabled = False
+        btnCancel.Enabled = True
+        lblPercent.Text = "0%"
+        tokenSource = New CancellationTokenSource()
+
+        Try
+            Dim allFiles As Integer = Await AllSubfolderFiles(sLocation, progressIndicator, tokenSource.Token)
+            Debug.WriteLine(allFiles.ToString())        'the number of subfolders iterated
+        Catch ex As OperationCanceledException
+            'do stuff when cancelled
+            lblPercent.Text = "Cancelled"
+        End Try
+
+        btnStart.Enabled = True
+        btnCancel.Enabled = False
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        btnCancel.Enabled = False
+        btnStart.Enabled = False
+        tokenSource.Cancel()
+    End Sub
+
+    Private Async Function AllSubfolderFiles(location As String, progress As IProgress(Of Integer),
+                                             token As CancellationToken) As Task(Of Integer)
+        Dim dirsTotal As Integer = Directory.GetDirectories(location).Length
+        Dim dirsFraction As Integer = Await Task(Of Integer).Run(
+            Function()
+                Dim counter As Integer = 0
+                For Each subDir As String In Directory.GetDirectories(location)
+                    SubfolderFiles(subDir)
+                    counter += 1
+                    token.ThrowIfCancellationRequested()
+                    If progress IsNot Nothing Then
+                        progress.Report(counter * 100 / dirsTotal)
+                    End If
+                Next
+
+                Return counter
+            End Function
+            )
+        Return dirsFraction
+    End Function
+
+    Private Sub UpdateProgress(value As Integer)
+        barFileProgress.Value = value
+        lblPercent.Text = (value / 100).ToString("#0.##%")
+    End Sub
+
+    Private Sub SubfolderFiles(location As String)
+        'source: http://stackoverflow.com/questions/16237291/visual-basic-2010-continue-on-error-unauthorizedaccessexception#answer-16237749
+
+        Dim paths = New Queue(Of String)()
+        Dim fileNames = New List(Of String)()
+
+        paths.Enqueue(location)
+
+        While paths.Count > 0
+            Dim sDir = paths.Dequeue()
+
+            Try
+                Dim files = Directory.GetFiles(sDir)
+                For Each file As String In Directory.GetFiles(sDir)
+                    fileNames.Add(file)
+                Next
+
+                For Each subDir As String In Directory.GetDirectories(sDir)
+                    paths.Enqueue(subDir)
+                Next
+            Catch ex As UnauthorizedAccessException
+                ' log the exception or ignore it
+                Debug.WriteLine("Directory {0}  could not be accessed!", sDir)
+            Catch ex As PathTooLongException
+                'bypass
+                '**** handle it in the future!
+            Catch ex As Exception
+                ' log the exception or ...
+                Throw
+            End Try
+        End While
+        'could return fileNames collection
+    End Sub
+
+
+End Class
